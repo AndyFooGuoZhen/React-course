7f55f12dd0d733355b8249bb6911305ac9e2f9b6
AndyFooGuoZhen <ndfucoding@gmail.com>
2022-01-19 15:36:15 -0600

    Added NavBar

--- Portfolio/capture/src/App.js
+++ Portfolio/capture/src/App.js
@@ -1,8 +1,14 @@
+import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
+
+//Global style
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
--- Portfolio/capture/src/components/AboutSection.js
+++ Portfolio/capture/src/components/AboutSection.js
@@ -1,33 +1,37 @@
 import home1 from "../img/home1.png";
+import styled from "styled-components";
+import { About, Description, Image, Hide } from "../style";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
         </p>
         <button>Contact us</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img src={home1} alt="guy with a camera" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
+
+//Styled components
 
 export default AboutSection;
